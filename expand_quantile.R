90cfc816738b2d2abfd0ca39c9b32308b4b77d69
cordura21 <cordura21@gmail.com>
2017-07-03 14:09:19 -0300

    Update expand_quantile.R

--- expand_quantile.R
+++ expand_quantile.R
@@ -1,13 +1,1 @@
-expand_quantile <- function(x, p, skip.first,...){
-  x.length <- length(x)
-  if(x.length < skip.first){
-    return(rep(NA,x.length))
-  }
-  exp_quantile_vector <- vector(length = x.length)
-  exp_quantile_vector[1:x.length] <- NA
-  
-  for(iLoop in skip.first:length(x)){
-    exp_quantile_vector[iLoop] <- quantile(x[1:iLoop],p = p[1])
-  }
-  return(exp_quantile_vector)
-} 
+
